8ad25b85043c8461b6fe36b8434fd7819839417b
prana <prakashrana9847067614@gmail.com>
2020-03-04 17:10:59 -0500

    Added password util

---
+++ C0773839_MidTerm_MAD3115W2020/Helper/PasswordUtil.swift
@@ -0,0 +1,27 @@
+//
+//  PasswordUtil.swift
+//  C0773839_MidTerm_MAD3115W2020
+//
+//  Created by Prakash on 2020-03-04.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+
+struct PasswordUtil {
+    
+    static var randomStr = "12344564576zxfdfsdfasdca"
+    static func getSalt() -> String{
+        return PasswordUtil.randomStr
+    }
+    
+    static func getHash(plainPassword: String, salt: String) -> String{
+        return plainPassword + salt
+    }
+    
+    static func validate(plainPassword: String, salt: String, hashPassword: String) -> Bool{
+        return getHash(plainPassword: plainPassword, salt: salt) ==  hashPassword
+    }
+    
+}
