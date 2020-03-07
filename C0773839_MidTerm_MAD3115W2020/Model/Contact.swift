425ce51eee03078d62c93a6d0888eccdfe5817a6
prana <prakashrana9847067614@gmail.com>
2020-03-06 20:22:46 -0500

    removed extra code

--- C0773839_MidTerm_MAD3115W2020/Model/Contact.swift
+++ C0773839_MidTerm_MAD3115W2020/Model/Contact.swift
@@ -16,33 +16,7 @@ public class Contact
     private var mobileNumber: String
     private var emailId: String
     private var address: Address
-    
-    
-    /* Function to validate the email id */
-    
-    public func isValid(emailAddressString: String) -> Bool
-     {
-         var returnValue = true
-         let emailRegEx = "[A-Z0-9a-z.-_]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,3}"
-    do
-     {
-         let regex = try NSRegularExpression(pattern: emailRegEx)
-         let nsString = emailAddressString as NSString
-         let results = regex.matches(in: emailAddressString, range: NSRange(location: 0, length: nsString.length))
-         
-         if results.count == 0
-         {
-             returnValue = false
-         }
-     }
-     catch let error as NSError
-     {
-         print("invalid regex: \(error.localizedDescription)")
-         returnValue = false
-     }
-     
-     return  returnValue
-     }
+
 
     
     internal init(mobileNumber: String, emailId: String, address: Address) throws
