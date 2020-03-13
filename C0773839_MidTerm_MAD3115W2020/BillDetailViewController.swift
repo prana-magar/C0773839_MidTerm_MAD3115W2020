5098691cb8422e3ccba62773fe4d00c4225aa6a0
prana <prakashrana9847067614@gmail.com>
2020-03-13 09:23:59 -0400

    Added view controller for add bill

---
+++ C0773839_MidTerm_MAD3115W2020/BillDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BillDetailViewController.swift
+//  C0773839_MidTerm_MAD3115W2020
+//
+//  Created by Prakash on 2020-03-13.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import UIKit
+
+class BillDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
