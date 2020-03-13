b8f857469f08bfebf95d95278854fdc52ddbcb18
prana <prakashrana9847067614@gmail.com>
2020-03-13 11:54:53 -0400

    Added outlets

---
+++ C0773839_MidTerm_MAD3115W2020/AddBillViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AddBillViewController.swift
+//  C0773839_MidTerm_MAD3115W2020
+//
+//  Created by Prakash on 2020-03-13.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import UIKit
+
+class AddBillViewController: UIViewController {
+
+    @IBOutlet weak var labelBillType: UITextField!
+    
+    @IBOutlet weak var labelBillAmount: UITextField!
+    @IBOutlet weak var labelBillDate: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
